0676b2d8c2cb3d192ed236ebb51df6e746252764
Sashi Gandavarapu <sasanka.gandavarapu@gmail.com>
2017-04-05 20:30:34 -0700

    file to download indices

---
+++ download.sh
@@ -0,0 +1,66 @@
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2017-04 -p 4 *.bestbuy.com -j -z -o 2017-04-bestbuy;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2017-04 -p 4 *.walmart.com -j -z -o 2017-04-walmart;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2017-04 -p 4 *.amazon.com -j -z -o 2017-04-amazon;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2017-04 -p 4 *.homedepot.com -j -z -o 2017-04-homedepot;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2017-04 -p 4 *.lowes.com -j -z -o 2017-04-lowes;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2017-04 -p 4 *.menards.com -j -z -o 2017-04-menards;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2017-04 -p 4 *.wayfair.com -j -z -o 2017-04-wayfair;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2017-04 -p 4 *.frys.com -j -z -o 2017-04-frys;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2017-04 -p 4 *.TigerDirect.com -j -z -o 2017-04-tigerdirect;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2017-04 -p 4 *.costco.com -j -z -o 2017-04-costco;
+
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-50 -p 4 *.bestbuy.com -j -z -o 2016-50-bestbuy;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-50 -p 4 *.walmart.com -j -z -o 2016-50-walmart;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-50 -p 4 *.amazon.com -j -z -o 2016-50-amazon;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-50 -p 4 *.homedepot.com -j -z -o 2016-50-homedepot;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-50 -p 4 *.lowes.com -j -z -o 2016-50-lowes;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-50 -p 4 *.menards.com -j -z -o 2016-50-menards;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-50 -p 4 *.wayfair.com -j -z -o 2016-50-wayfair;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-50 -p 4 *.frys.com -j -z -o 2016-50-frys;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-50 -p 4 *.TigerDirect.com -j -z -o 2016-50-tigerdirect;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-50 -p 4 *.costco.com -j -z -o 2016-50-costco;
+
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-44 -p 4 *.bestbuy.com -j -z -o 2016-44-bestbuy;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-44 -p 4 *.walmart.com -j -z -o 2016-44-walmart;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-44 -p 4 *.amazon.com -j -z -o 2016-44-amazon;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-44 -p 4 *.homedepot.com -j -z -o 2016-44-homedepot;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-44 -p 4 *.lowes.com -j -z -o 2016-44-lowes;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-44 -p 4 *.menards.com -j -z -o 2016-44-menards;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-44 -p 4 *.wayfair.com -j -z -o 2016-44-wayfair;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-44 -p 4 *.frys.com -j -z -o 2016-44-frys;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-44 -p 4 *.TigerDirect.com -j -z -o 2016-44-tigerdirect;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-44 -p 4 *.costco.com -j -z -o 2016-44-costco;
+
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-40 -p 4 *.bestbuy.com -j -z -o 2016-40-bestbuy;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-40 -p 4 *.walmart.com -j -z -o 2016-40-walmart;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-40 -p 4 *.amazon.com -j -z -o 2016-40-amazon;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-40 -p 4 *.homedepot.com -j -z -o 2016-40-homedepot;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-40 -p 4 *.lowes.com -j -z -o 2016-40-lowes;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-40 -p 4 *.menards.com -j -z -o 2016-40-menards;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-40 -p 4 *.wayfair.com -j -z -o 2016-40-wayfair;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-40 -p 4 *.frys.com -j -z -o 2016-40-frys;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-40 -p 4 *.TigerDirect.com -j -z -o 2016-40-tigerdirect;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-40 -p 4 *.costco.com -j -z -o 2016-40-tigerdirect;
+
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-36 -p 4 *.bestbuy.com -j -z -o 2016-36-bestbuy;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-36 -p 4 *.walmart.com -j -z -o 2016-36-walmart;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-36 -p 4 *.amazon.com -j -z -o 2016-36-amazon;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-36 -p 4 *.homedepot.com -j -z -o 2016-36-homedepot;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-36 -p 4 *.lowes.com -j -z -o 2016-36-lowes;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-36 -p 4 *.menards.com -j -z -o 2016-36-menards;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-36 -p 4 *.wayfair.com -j -z -o 2016-36-wayfair;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-36 -p 4 *.frys.com -j -z -o 2016-36-frys;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-36 -p 4 *.TigerDirect.com -j -z -o 2016-36-tigerdirect;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-36 -p 4 *.costco.com -j -z -o 2016-36-costco;
+
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-30 -p 4 *.bestbuy.com -j -z -o 2016-30-bestbuy;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-30 -p 4 *.walmart.com -j -z -o 2016-30-walmart;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-30 -p 4 *.amazon.com -j -z -o 2016-30-amazon;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-30 -p 4 *.homedepot.com -j -z -o 2016-30-homedepot;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-30 -p 4 *.lowes.com -j -z -o 2016-30-lowes;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-30 -p 4 *.menards.com -j -z -o 2016-30-menards;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-30 -p 4 *.wayfair.com -j -z -o 2016-30-wayfair;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-30 -p 4 *.frys.com -j -z -o 2016-30-frys;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-30 -p 4 *.TigerDirect.com -j -z -o 2016-30-tigerdirect;
+../cdx-index-client/cdx-index-client.py -c CC-MAIN-2016-30 -p 4 *.costco.com -j -z -o 2016-30-costco
+
