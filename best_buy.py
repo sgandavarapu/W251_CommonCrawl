ca757c21f4907c569435e15e623f334ad31340f4
sho-ohata <sho.ohata@gmail.com>
2017-04-09 17:39:33 -0400

    update for bestbuy

--- best_buy.py
+++ best_buy.py
@@ -29,32 +29,32 @@ def get_bestbuy_files(month,folder_path):
                     try:
                         response = get_cc_record(record)
                         parser = BeautifulSoup(response, "lxml")
-                    except ConnectionError as e: 
-                        parser = 'no response'
-
-                    if parser.find_all("div", class_="item-price") != []:
-                        product_name = parser.find("title").renderContents()
-                        product[product_name] = {
-                        'url': record['url'],
-                        'price': parser.find("div", class_="item-price").text.replace('\n', ''),
-                        'timestamp': record['timestamp']
-                        }
                     
-                    for line in re.findall('{(.*?)}', response):
-                        if line[0:8] == 'upc_code':
-                            try:
-                                category = (re.findall('(?<=category:)(.*?)(?=,)', line)[0]).strip()
-                                category = re.sub("\'", '', category)
-                                product[product_name]['category'] = category
-                            except KeyError:
-                                continue
-                            try:
-                                subcategory = (re.findall('(?<=subcategory:)(.*?)(?=,)', line)[0]).strip()
-                                subcategory = re.sub("\'", '', subcategory)
-                                product[product_name]['subcategory'] = subcategory
-                            except KeyError:
-                                continue
-                                    
+                        if parser.find_all("div", class_="item-price") != []:
+                            product_name = parser.find("title").renderContents()
+                            product[product_name] = {
+                            'url': record['url'],
+                            'price': parser.find("div", class_="item-price").text.replace('\n', ''),
+                            'timestamp': record['timestamp']
+                            }
+                        
+                        for line in re.findall('{(.*?)}', response):
+                            if line[0:8] == 'upc_code':
+                                try:
+                                    category = (re.findall('(?<=category:)(.*?)(?=,)', line)[0]).strip()
+                                    category = re.sub("\'", '', category)
+                                    product[product_name]['category'] = category
+                                except KeyError:
+                                    continue
+                                try:
+                                    subcategory = (re.findall('(?<=subcategory:)(.*?)(?=,)', line)[0]).strip()
+                                    subcategory = re.sub("\'", '', subcategory)
+                                    product[product_name]['subcategory'] = subcategory
+                                except KeyError:
+                                    continue
+                    except: 
+                        next
+
         with open(each_file[:-3]+'_processed_products.csv','wb') as csv_file:
             writer = csv.writer(csv_file)
             attrs=[]
